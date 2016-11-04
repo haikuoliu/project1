5b0edff12fc2c983954303b7ba507dea5b8dee40
haikuoliu <haikuo.liu.cu@gmail.com>
2016-11-04 03:00:51 -0400

    sth

--- routes/event.py
+++ routes/event.py
@@ -16,14 +16,13 @@ def view_user_profile():
             event_type = request.form['event_type']
             description = request.form['description']
             uid = request.form['uid']
-            eid = request.form['username']
-            eid = request.form['topics']
+            topics = request.form['topics']
             url = request.form['url']
             content = request.form['content']
             title = request.form['title']
             ret = {}
             ret[STATUS] = SUCCESS
-            if eid == null: # create
+            if eid == null:  # create
                 if event_type == "blog":
                     exe_sql = "INSERT INTO events(time, event_type, description, uid, title, content) " \
                               "VALUES(now(), %s, %s, %s, %s, %s)"
@@ -32,11 +31,16 @@ def view_user_profile():
                     exe_sql = "INSERT INTO events(time, event_type, description, uid, title, url) " \
                               "VALUES(now(), %s, %s, %s, %s, %s)"
                     conn.execute(exe_sql, (event_type, description, uid, title, url))
+                    exe_sql = "SELECT eid FROM events ORDER BY eid DESC LIMIT 1"
+                    res = conn.execute(exe_sql)
+                    row = res.fecthone()
+                    print row
+                    res[RESULT] = {"eid": row["eid"]}
                 else:
                     raise Exception("The event type should be either blog or picture")
                 print ret
                 return json.dumps(ret)
-            else: # edit
+            else:  # edit
                 pass
 
         except Exception, e:
